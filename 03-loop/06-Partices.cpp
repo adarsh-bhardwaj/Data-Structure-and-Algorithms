ba9d202ab2b5a6ccf928d31abba460d35026e253
Adarsh Bhardwaj <adarshbhardwaj285@gmail.com>
2022-07-10 21:44:14 +0530

    Few more pattern Added

---
+++ 03-loop/06-Partices.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int a =24;
+
+    if(a>20){
+        cout<<"Hello";
+    }
+    else if(a == 24){
+        cout<<"24";
+    }
+    else{
+        cout<<"Default";
+    }
+
+    return 0;
+}
