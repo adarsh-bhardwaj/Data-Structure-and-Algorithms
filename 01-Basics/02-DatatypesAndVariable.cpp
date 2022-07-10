be7ff37026d1c1fa6f6d34b8a8eafc2df23481bb
Adarsh Bhardwaj <adarshbhardwaj285@gmail.com>
2022-07-10 13:53:51 +0530

    Basic Program

---
+++ 01-Basics/02-DatatypesAndVariable.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int a =291;
+    cout<< a<<endl;
+    
+    char ch = 'v';
+    cout<<ch<<endl;
+
+    bool bl = true;
+    cout<<bl<<endl;
+
+    float fl = 2.4;
+    cout<<fl<<endl;
+
+    double db = 3.212;
+    cout<< db <<endl;
+
+    int size = sizeof(fl);
+    cout<<"Size is: "<<size;
+
+    return 0;
+}
