b90b90dbfb846fabe3032ffdb3b5ba33546ea5e5
Adarsh Bhardwaj <adarshbhardwaj285@gmail.com>
2022-07-12 12:22:38 +0530

    Start Of Day 12-7-2022

---
+++ 05-ForLoop/01-Basic.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    cout<<"Counting Will Print Below: "<<endl;
+    for(int i=1;i<=n;i++){
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
